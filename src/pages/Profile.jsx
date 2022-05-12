4e84a0ad89f33ce795e617b32bd6bb7b04e35bec
Brandon Franks <bgfranks.dev@gmail.com>
2022-05-11 21:59:33 -0400

    added the ui on the profile to show the logged in users username and email. also added the functionality to change the display name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,95 @@
-import { useState, useEffect } from 'react'
-import { getAuth } from 'firebase/auth'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 
 export default function Profile() {
-  const [user, setUser] = useState(null)
-
   const auth = getAuth()
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-  return user ? <h1>{user.displayName}</h1> : 'Not logged in'
+  const { name, email } = formData
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+
+        //update display name in firesote
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name,
+        })
+      }
+    } catch (error) {
+      toast.error('Error updating account information')
+    }
+  }
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  const handleLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  return (
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={handleLogout}>
+          Log Out
+        </button>
+      </header>
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Account Information</p>
+          <p
+            className='changePersonalDetails'
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='email'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
+    </div>
+  )
 }
